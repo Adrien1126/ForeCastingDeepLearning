b0102908c51ad0fe7c09826c4fb0472a6b0951fb
Charles Yang <charles.yang@ensiie.eu>
2024-12-16 00:12:04 +0100

    deep cnn

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, Input, LSTM
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, Input, LSTM, BatchNormalization, Activation
 from tensorflow.keras.regularizers import l2
 
 def build_cnn1d_model(input_shape):
@@ -27,7 +27,42 @@ def build_cnn1d_model(input_shape):
     model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
-def build_stronger_cnn(input_shape):
+def build_cnn1d_deep_model(input_shape):
+    model = Sequential()
+    model.add(Input(shape=input_shape))
+
+    # 1st Convolutional Block
+    model.add(Conv1D(filters=128, kernel_size=5, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Dropout(0.3))
+
+    # 2nd Convolutional Block
+    model.add(Conv1D(filters=128, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size=2))  # Stop here for pooling
+    model.add(Dropout(0.3))
+
+    # 3rd Convolutional Block - NO Pooling
+    model.add(Conv1D(filters=64, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+
+    # Fully Connected Layers
+    model.add(Flatten())
+    model.add(Dense(units=100, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(units=1))  # Output Layer
+
+    # Compile
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+
+def build_lstm_cnn(input_shape):
     model = Sequential()
 
     model.add(Input(shape=input_shape))
